f4e9c8b83fdfdb52bff8edddeb4afa513ea14fb5
Xiangqi Li <xiangqi@cs.utah.edu>
2014-12-01 15:00:38 -0700

    organize browser

--- pkgs/medic/browser.rkt
+++ pkgs/medic/browser.rkt
@@ -35,77 +35,7 @@
 (define widget%
   (class object%
     (init parent)
-           
     (super-new)
-   
-    (define split-panel (new panel:horizontal-dragable% [parent parent]))
-    (define log-panel (new vertical-panel% [parent split-panel]))
-    (define right-panel (new panel:vertical-dragable% [parent split-panel]))
-    (define top-right-panel (new panel:horizontal-dragable% [parent right-panel]))
-    (define graph-panel (new vertical-panel% [parent top-right-panel]))
-    (define aggre-panel (new vertical-panel% [parent top-right-panel]))
-    (define timeline-panel (new vertical-panel% [parent right-panel]))
-    
-    (send split-panel begin-container-sequence)
-    (send split-panel set-percentages (list 1/4 3/4))
-    (send split-panel end-container-sequence)
-    (send right-panel begin-container-sequence)
-    (send right-panel set-percentages (list 1/2 1/2))
-    (send right-panel end-container-sequence)
-    (send top-right-panel begin-container-sequence)
-    (send top-right-panel set-percentages (list 1/2 1/2))
-    (send top-right-panel end-container-sequence)
-    
-    
-    (define timeline-data (get-timeline-data))
-    #;(set! timeline-data 
-          (list 
-           (list "str1" #f (list "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f
-                                "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f))
-           (list "x1" #f (list 3 7 5 9 10))
-           (list "y1" #f (list -1 2 3 -4 5))
-           (list "z1" #f (list 5 5 5 5 5))
-           (list "x1" #f (list #f #f #f #f #t))
-           (list "x1 > 5" #t (list #f #t #f #t #t))
-           
-           (list "str2" #f (list "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f
-                                "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f))
-           (list "x2" #f (list 3 7 5 9 10))
-           (list "y2" #f (list -1 2 3 -4 5))
-           (list "z2" #f (list 5 5 5 5 5))
-           (list "x2" #f (list #f #f #f #f #t))
-           (list "x2 > 5" #t (list #f #t #f #t #t))
-           ))
-    (define max-length (apply max (map length (map third timeline-data))))
-    
-    (define slider-panel (new horizontal-panel% 
-                              [parent timeline-panel] 
-                              [stretchable-height #f]
-                              [style '(border)]))
-    (define prefix "Timeline\n")
-    (define slider-message (new message% 
-                                [label (string-append prefix (format "~a/~a" 0 max-length))] 
-                                [parent slider-panel]))
-    
-    (define (on-step slider event)
-      (define cur (send slider get-value))
-      (send slider-message set-label (string-append prefix (format "~a/~a" cur max-length)))
-      (send timeline-canvas scrutinize cur))
-    
-    (define slider (new slider% 
-                        [label ""] 
-                        [min-value 0] 
-                        [max-value max-length] 
-                        [parent slider-panel]
-                        [callback on-step]))
-    
-    (define-values (graph-width graph-height) (send graph-panel get-size))
-    (define graph-pb (new graph-pasteboard%
-                          [raw-edges (get-raw-edges)]
-                          [width graph-width]
-                          [height graph-height]))
-    
-    (define log-text (new log-text% [data (get-log-data)]))
     
     (define/private (on-check-box-value-change c)
       (define label (send c get-label))
@@ -121,10 +51,12 @@
       (for-each (lambda (b) (send b set-value #f)) boxes)
       (send log-text unhighlight-all-text))
     
-    (define layer-frame #f)
-    (define layer-panel #f)
+    (define (on-step slider event)
+      (define cur (send slider get-value))
+      (send slider-message set-label (string-append prefix (format "~a/~a" cur max-length)))
+      (send timeline-canvas scrutinize cur))
     
-    (define (initialize-layer-frame)
+    (define/private (initialize-layer-frame)
       (set! layer-frame (new frame% 
                              [label "Layer Viewer"]
                              [width 400]
@@ -154,10 +86,29 @@
       (unless (send layer-frame is-shown?)
         (send layer-frame show #t)))
     
+    ;; Main Layout
+    (define split-panel (new panel:horizontal-dragable% [parent parent]))
+    (define log-panel (new vertical-panel% [parent split-panel]))
+    (define right-panel (new panel:vertical-dragable% [parent split-panel]))
+    (define top-right-panel (new panel:horizontal-dragable% [parent right-panel]))
+    (define graph-panel (new vertical-panel% [parent top-right-panel]))
+    (define aggre-panel (new vertical-panel% [parent top-right-panel]))
+    (define timeline-panel (new vertical-panel% [parent right-panel]))
+    
+    (send split-panel begin-container-sequence)
+    (send split-panel set-percentages (list 1/4 3/4))
+    (send split-panel end-container-sequence)
+    (send right-panel begin-container-sequence)
+    (send right-panel set-percentages (list 1/2 1/2))
+    (send right-panel end-container-sequence)
+    (send top-right-panel begin-container-sequence)
+    (send top-right-panel set-percentages (list 1/2 1/2))
+    (send top-right-panel end-container-sequence)
+    
+    ;; Log
     (define medic-bitmap 
       (compiled-bitmap (pict->bitmap (cc-superimpose (colorize (filled-rectangle 18 6) "red")
                                                      (colorize (filled-rectangle 6 18) "red")))))
-                                           
     (new switchable-button%
          [label "Log Viewer"]
          [bitmap medic-bitmap]
@@ -165,31 +116,74 @@
          [vertical-tight? #t]
          [parent log-panel]
          [callback (lambda (b) (popup-layer-viewer))])
-    
+    (define log-text (new log-text% [data (get-log-data)]))
+    (define layer-frame #f)
+    (define layer-panel #f)
     (new editor-canvas% 
          [parent log-panel]
          [editor log-text]
          [style '(no-focus auto-hscroll auto-vscroll)])
     
-    (define timeline-canvas (new timeline-canvas%
-                                 [data timeline-data]
-                                 [parent timeline-panel]
-                                 [style '(hscroll vscroll)]))
-    
-    (send timeline-canvas init-auto-scrollbars 
-          (send timeline-canvas get-actual-width) 
-          (send timeline-canvas get-actual-height)
-          0.0 0.0)
-    (send timeline-canvas show-scrollbars #t #t)
+    ;; Graph
     (new message% [parent graph-panel] [label "Graph"])
+    (define-values (graph-width graph-height) (send graph-panel get-size))
+    (define graph-pb (new graph-pasteboard%
+                          [raw-edges (get-raw-edges)]
+                          [width graph-width]
+                          [height graph-height]))
     (new editor-canvas%
          [parent graph-panel]
          [editor graph-pb])
     
+    ;; Aggregate
     (new message% [parent aggre-panel] [label "Aggregate"])
-    
     (define aggre-text (new aggregate-editor% [data (get-aggregate-data)]))
     (new editor-canvas%
          [parent aggre-panel]
-         [editor aggre-text])))
+         [editor aggre-text])
+    
+    ;; Timeline
+    (define timeline-data (get-timeline-data))
+    #;(set! timeline-data 
+          (list 
+           (list "str1" #f (list "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f
+                                "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f))
+           (list "x1" #f (list 3 7 5 9 10))
+           (list "y1" #f (list -1 2 3 -4 5))
+           (list "z1" #f (list 5 5 5 5 5))
+           (list "x1" #f (list #f #f #f #f #t))
+           (list "x1 > 5" #t (list #f #t #f #t #t))
+           
+           (list "str2" #f (list "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f
+                                "hello" "world" 1 #t "time line" "core" "canvas" "to" 'started 'loop 'download 'it 6 9 #t #f))
+           (list "x2" #f (list 3 7 5 9 10))
+           (list "y2" #f (list -1 2 3 -4 5))
+           (list "z2" #f (list 5 5 5 5 5))
+           (list "x2" #f (list #f #f #f #f #t))
+           (list "x2 > 5" #t (list #f #t #f #t #t))
+           ))
+    (define max-length (apply max (map length (map third timeline-data))))
+    (define slider-panel (new horizontal-panel% 
+                              [parent timeline-panel] 
+                              [stretchable-height #f]
+                              [style '(border)]))
+    (define prefix "Timeline\n")
+    (define slider-message (new message% 
+                                [label (string-append prefix (format "~a/~a" 0 max-length))] 
+                                [parent slider-panel]))
+    (define slider (new slider% 
+                        [label ""] 
+                        [min-value 0] 
+                        [max-value max-length] 
+                        [parent slider-panel]
+                        [callback on-step]))
+    (define timeline-canvas (new timeline-canvas%
+                                 [data timeline-data]
+                                 [parent timeline-panel]
+                                 [style '(hscroll vscroll)]))
+    (send timeline-canvas init-auto-scrollbars 
+          (send timeline-canvas get-actual-width) 
+          (send timeline-canvas get-actual-height)
+          0.0 0.0)
+    (send timeline-canvas show-scrollbars #t #t)))
     
