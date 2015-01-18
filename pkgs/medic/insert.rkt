55694c0a2bb34197058cf4179b0570cc390fa965
Xiangqi Li <xiangqi@cs.utah.edu>
2015-01-17 17:55:37 -0700

    rm print

--- pkgs/medic/insert.rkt
+++ pkgs/medic/insert.rkt
@@ -41,9 +41,6 @@
       [else stx]))
   
   (define (insert-stx stx insert-table at-table)
-    (printf "insert-table=~v\n" insert-table)
-    (printf "at-table=~v\n" at-table)
-    (printf "insert-stx: stx=~v\n" stx)
     (define top-level-ids '())
     (define let-exit '())
     (define internal-let? #f)
